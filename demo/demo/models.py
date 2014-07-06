9d23ce5a925cf5d865dd18dd3c95c7b83fef824e
gotlium <gotlium@gmail.com>
2014-07-07 04:40:04 +0500

    demo success signal

--- demo/demo/models.py
+++ demo/demo/models.py
@@ -1,5 +1,5 @@
 from django.forms.models import model_to_dict
-from g2s.signals import g2s_signal
+from g2s.signals import g2s_payment_was_successful
 from pprint import pprint
 
 
@@ -8,5 +8,5 @@ def transaction_result(sender, **kwargs):
         pprint(model_to_dict(sender))
 
 
-g2s_signal.connect(
+g2s_payment_was_successful.connect(
     transaction_result, dispatch_uid="demo.models.transaction_result")
